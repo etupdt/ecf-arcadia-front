46f62c60718eef9d5c7746b843f30b41a5ded985
Denis Tavernier <denis.tavernier@gamil.com>
2024-03-31 10:12:21 +0200

    Render: reformat views disposition

--- src/app/pages/components/view-form/view-form.component.ts
+++ src/app/pages/components/view-form/view-form.component.ts
@@ -1,40 +1,65 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { View } from 'src/app/interfaces/View';
-import { FormsModule } from '@angular/forms';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ViewService } from 'src/app/services/view.service';
 
 @Component({
     selector: 'app-view-form',
     templateUrl: './view-form.component.html',
     styleUrls: ['./view-form.component.scss'],
     standalone: true,
-    imports: [FormsModule]
+    imports: [FormsModule, ReactiveFormsModule]
 })
-export class ViewFormComponent {
+export class ViewFormComponent implements OnInit {
 
-    isUnChanged : boolean = true
-    
-    initForm = (): View => {
-        return {
-          id : 0,
-          pseudo : "",
-          comment : "",
-          isVisible : false
-        }
-    }
-    
-    @Input() view: View = this.initForm()
-    
+    constructor (
+        private viewService: ViewService
+    ) {}
+
+    @Input() view!: View
+    @Input() updatable: Boolean = false
+
     @Output() return: EventEmitter<View> = new EventEmitter()
     
-    save(): void {
-        if (!this.isUnChanged) {
-            this.return.emit(this.view)
-            this.view = this.initForm()
+    viewForm!: FormGroup
+
+    updated: boolean = false
+
+    ngOnInit(): void {
+        this.viewForm = new FormGroup({
+            pseudo: new FormControl(this.view.pseudo, Validators.required),
+            comment: new FormControl(this.view.comment, Validators.required),
+        });    
+        if (!this.updatable) {
+            this.pseudo.disable()
+            this.comment.disable()
         }
+        this.viewForm.valueChanges.subscribe(change => {
+            this.updated = this.isUpdated()
+        })        
+
+    }        
+
+    save(): void {
+
+        this.view.pseudo = this.pseudo.value
+        this.view.comment = this.comment.value
+
+        this.viewService.putView(this.view).subscribe({
+            next: (res) => {
+            },
+            error: (error: { error: { message: any; }; }) => {
+            }
+        })
+
     }
 
-    change () {
-        this.isUnChanged = (this.view.pseudo === "" || this.view.comment === "")
+    isUpdated = () => {
+        return this.view.pseudo !== this.pseudo.value ||
+               this.view.comment !== this.comment.value
     }
+
+    get pseudo() { return this.viewForm.get('pseudo')! as FormControl }
+    get comment() { return this.viewForm.get('comment')! as FormControl }
 
 }
