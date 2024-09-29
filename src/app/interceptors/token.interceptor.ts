0c5e733929b3ca2a945dbf41266c2f792a09b8ea
Denis Tavernier <denis.tavernier@gamil.com>
2024-09-29 19:32:58 +0200

    Refactor: service authentification

--- src/app/interceptors/token.interceptor.ts
+++ src/app/interceptors/token.interceptor.ts
@@ -1,7 +1,7 @@
 import { HttpErrorResponse, HttpInterceptorFn, HttpResponse } from '@angular/common/http';
 import { JwtHelperService } from '@auth0/angular-jwt';
 import { inject } from '@angular/core';
-import { catchError, tap } from 'rxjs';
+import { catchError, switchMap, tap } from 'rxjs';
 import { ErrorModalComponent } from '../modals/error-modal/error-modal.component';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { AuthService } from '../services/auth.service';
@@ -13,51 +13,31 @@ export const tokenInterceptor: HttpInterceptorFn = (req, next) => {
     const authService: AuthService = inject(AuthService);
     const modalService: NgbModal = inject(NgbModal)
     const toastsService: ToastsService = inject(ToastsService)
-
-    let tokens
-
-    try {
-        tokens = JSON.parse(localStorage.getItem('arcadia_tokens')!)
-    } catch (error: any) {
-        if (localStorage.getItem('arcadia_tokens')) {
-            localStorage.removeItem('arcadia_tokens')
-        }
-    }
     
-    console.log('aller', req.url)
-
+    let access_token
+    
+    try {
+        access_token = localStorage.getItem('access_token')
+    } catch (error: any) {
+        if (localStorage.getItem('access_token')) {
+            localStorage.removeItem('access_token')
+        }    
+    }    
+    
     let modifiedReq = req
 
-    if (tokens) {
-        if (Date.now() > helper.decodeToken(tokens.access_token).exp * 1000) {
-            localStorage.removeItem('arcadia_tokens')
-            authService.refreshToken(tokens.refresh_token).subscribe({
-                next: (res: any) => {
-                    localStorage.setItem('arcadia_tokens', JSON.stringify(res))
-                    toastsService.show('Votre connexion a été prolongée !', 2000)
-                    modifiedReq = req.clone({
-                        headers: req.headers.set('Authorization', `Bearer ${res.access_token}`),
-                    })
-                    console.log(modifiedReq.url, modifiedReq.headers)
-                    authService.endWarning = false
-                },
-                error: (error: any) => {
-                    authService.logout()
-                    toastsService.show('Erreur de connexion ou connexion expirée, veuillez réessayer de vous reconnecter !', 2000)
-                    throw error
-                }
-            })
-        } else {
-            modifiedReq = req.clone({
-                headers: req.headers.set('Authorization', `Bearer ${tokens.access_token}`),
-            })
-        }
-    }
-   
+    if (access_token) {
+        modifiedReq = req.clone({
+            headers: req.headers.set('Authorization', `Bearer ${access_token}`),
+        })    
+    }    
+    
     return next(modifiedReq).pipe(
         tap((r: any)  => console.log('retour', r.url)),
         catchError((error: HttpErrorResponse) => {
+
             let message: string = ''
+
             if (error) {
                 switch (error.status) {
                     case 400: {
@@ -65,11 +45,20 @@ export const tokenInterceptor: HttpInterceptorFn = (req, next) => {
                         break;
                     }
                     case 401: {
-                        message = 'Email ou password incorrect !'
+                        message = ''
+                        authService.refreshToken().pipe(
+                            switchMap((res: any) => {
+                                return next(req.clone({
+                                    setHeaders: {
+                                      Authorization: `Bearer ${res.access_token}`
+                                    }
+                                }))
+                            })
+                        )
                         break;
                     }
                     case 403: {
-                        message = 'Habilitations insuffisantes pour effectuer cette opération !'
+                        message = "Vous n'êtes pas habilité à effectuer cette opération"
                         break;
                     }
                     case 500: {
@@ -82,13 +71,15 @@ export const tokenInterceptor: HttpInterceptorFn = (req, next) => {
                     }   
                 }
             }
-
-            const modal = modalService.open(ErrorModalComponent)
-            modal.componentInstance.message = message;
+        
+            if (message !== '') {
+                const modal = modalService.open(ErrorModalComponent)
+                modal.componentInstance.message = message;
+            }
 
             throw error
-
+    
         })
     )
-
+       
 }
