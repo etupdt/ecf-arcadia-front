1889113a04f49bb20cfebb90fef3f6c5445fc41c
Denis Tavernier <denis.tavernier@gamil.com>
2024-10-17 17:57:39 +0200

    Refactor: remove spec files

--- src/app/pipes/view-filter.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ViewFilterPipe } from './view-filter.pipe';
-
-describe('ViewFilterPipe', () => {
-  it('create an instance', () => {
-    const pipe = new ViewFilterPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
--- src/app/services/api.service.spec.ts
+++ src/app/services/api.service.spec.ts
@@ -1,16 +1,47 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { ApiService } from './api.service';
+import { User } from '../models/User';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 describe('ApiService', () => {
-  let service: ApiService;
+    let service: ApiService<User>;
+    let httpController: HttpTestingController;
+    let get: Observable<any>
+    const url = 'http://localhost:8080/'
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(ApiService);
-  });
+    let mock = User.deserialize({
+        "id": 1,
+        "firstname": "José",
+        "lastname": "Duseaux",
+        "email": "admin@test.com",
+        "role": "ADMIN"
+    }, 1)
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            imports: [ HttpClientTestingModule ]
+        });
+        service = TestBed.inject(ApiService<User>);
+        httpController = TestBed.inject(HttpTestingController)
+        get = service.getItem('user', 1)
+    });
+
+    it('should be read', () => {
+        service.getItem('users', 1).subscribe({
+            next: (result: User) => {
+                expect(result.firstname).toEqual('José');
+            }
+        })
+
+	    const req = httpController.expectOne({
+            method: 'GET',
+            url: `${url}ecf-arcadia-back/api/users/1`,
+        });
+  
+        req.flush(mock);
+      
+    })
+    
 });
