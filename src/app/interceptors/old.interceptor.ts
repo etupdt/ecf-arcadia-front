c87ba3e714ddd9e311120839e4b8b1137bbf78e2
Denis Tavernier <denis.tavernier@gamil.com>
2024-10-17 14:40:36 +0200

    Fix: display http errror message ko

--- src/app/interceptors/old.interceptor.ts
+++ src/app/interceptors/old.interceptor.ts
@@ -8,12 +8,16 @@ import {
 } from '@angular/common/http';
 import { catchError, defer, map, Observable, of, retry, switchMap, tap, timer } from 'rxjs';
 import { AuthService } from '../services/auth.service';
+import { ErrorModalComponent } from '../modals/error-modal/error-modal.component';
+import { NgModel } from '@angular/forms';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Injectable()
 export class OldInterceptor implements HttpInterceptor {
 
     constructor(
-        private authService: AuthService
+        private authService: AuthService,
+        private modalService: NgbModal,
     ) {}
 
     private isRefreshing = false;
@@ -38,7 +42,7 @@ export class OldInterceptor implements HttpInterceptor {
             })    
         }    
         
-            return next.handle(req).pipe(
+        return next.handle(req).pipe(
 
             catchError((error) => {
                 if (!req.url.match(/\/auth\/(refresh-token|authenticate)$/) &&
@@ -46,6 +50,8 @@ export class OldInterceptor implements HttpInterceptor {
                     error.status === 401
                 ) {
                     return this.handle401Error(req, next);
+                } else {
+                    this.displayErrorMessage(error)
                 }
         
                 throw error
@@ -95,4 +101,37 @@ export class OldInterceptor implements HttpInterceptor {
 
     }
         
+    private displayErrorMessage(error: HttpErrorResponse) {
+
+        let message: string = ''
+
+        switch (error.status) {
+            case 400: {
+                message = 'Erreur 400 !'
+                break;
+            }
+            case 401: {
+                message = 'Email ou password incorrect !'
+                break;
+            }
+            case 403: {
+                message = 'Habilitations insuffisantes pour effectuer cette opération !'
+                break;
+            }
+            case 500: {
+                message = 'Erreur du serveur !'
+                break;
+            }
+            default: {
+                message = 'Application indisponible !' 
+                break;
+            }   
+        
+        }
+    
+        const modal = this.modalService.open(ErrorModalComponent)
+        modal.componentInstance.message = message;
+    
+    }
+
 }
