6d343bc04a46c4921b19bd24a1eb19f53835ac7c
Denis Tavernier <denis.tavernier@gamil.com>
2024-10-18 15:24:41 +0200

    Test: Finalize naimal filter pipe tests

--- src/app/pipes/animal-filter.pipe.ts
+++ src/app/pipes/animal-filter.pipe.ts
@@ -10,7 +10,6 @@ export class AnimalFilterPipe implements PipeTransform {
 
     transform(animals: IAnimal[] | null, ...args: any[]): IAnimal[] {
 
-        console.log('filter animal', animals, args)
         let animalsToReturn = animals ? animals : []
 
         animalsToReturn = args[0] && args[0] !== '' ? animalsToReturn.filter(a => 
@@ -20,7 +19,6 @@ export class AnimalFilterPipe implements PipeTransform {
         animalsToReturn = args[1] && args[1].length > 0 ? _.intersectionBy(animalsToReturn, args[1], 'id') 
         : animalsToReturn.filter(a => a.veterinaryReports!.length > 0)
         
-        console.log('filter animalsToReturn', animalsToReturn)
         return animalsToReturn
 
     }
